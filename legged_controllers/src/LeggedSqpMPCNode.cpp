bb87e7b31abee4bf7e272a92942e936b048493f4
fishjohn <1258676867@qq.com>
2023-07-27 23:18:59 +0800

    add mpc raw output node

---
+++ legged_controllers/src/LeggedSqpMPCNode.cpp
@@ -0,0 +1,55 @@
+//
+// Created by luohx on 23-7-27.
+//
+
+#include <ros/init.h>
+#include <ros/package.h>
+
+#include <ocs2_legged_robot/LeggedRobotInterface.h>
+#include <ocs2_ros_interfaces/mpc/MPC_ROS_Interface.h>
+#include <ocs2_ros_interfaces/synchronized_module/RosReferenceManager.h>
+#include <ocs2_ros_interfaces/synchronized_module/SolverObserverRosCallbacks.h>
+#include <ocs2_sqp/SqpMpc.h>
+
+#include "ocs2_legged_robot_ros/gait/GaitReceiver.h"
+
+#include "legged_controllers/MotionGenerator.h"
+
+using namespace ocs2;
+using namespace legged_robot;
+
+int main(int argc, char** argv) {
+  const std::string robotName = "legged_robot";
+
+  // Initialize ros node
+  ::ros::init(argc, argv, robotName + "_mpc");
+  ::ros::NodeHandle nodeHandle;
+  // Get node parameters
+  std::string taskFile, urdfFile, referenceFile;
+  nodeHandle.getParam("/taskFile", taskFile);
+  nodeHandle.getParam("/urdfFile", urdfFile);
+  nodeHandle.getParam("/referenceFile", referenceFile);
+
+  // Robot interface
+  LeggedRobotInterface interface(taskFile, urdfFile, referenceFile);
+
+  // Gait receiver
+  auto gaitReceiverPtr =
+      std::make_shared<GaitReceiver>(nodeHandle, interface.getSwitchedModelReferenceManagerPtr()->getGaitSchedule(), robotName);
+
+  // ROS ReferenceManager
+  auto rosReferenceManagerPtr = std::make_shared<RosReferenceManager>(robotName, interface.getReferenceManagerPtr());
+  rosReferenceManagerPtr->subscribe(nodeHandle);
+
+  // MPC
+  SqpMpc mpc(interface.mpcSettings(), interface.sqpSettings(), interface.getOptimalControlProblem(), interface.getInitializer());
+  mpc.getSolverPtr()->setReferenceManager(rosReferenceManagerPtr);
+  mpc.getSolverPtr()->addSynchronizedModule(gaitReceiverPtr);
+
+  // Launch MPC ROS node
+  MPC_ROS_Interface mpcNode(mpc, robotName);
+  mpcNode.launchNodes(nodeHandle);
+
+  // Successful exit
+  return 0;
+}
