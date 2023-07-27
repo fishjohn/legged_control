8ba05610751f4a85ab0c15f0a1c4cee6e61cfa35
fishjohn <1258676867@qq.com>
2023-07-27 23:19:41 +0800

    add task list to motion data generator

--- legged_controllers/include/legged_controllers/MotionDataGenerator.h
+++ legged_controllers/include/legged_controllers/MotionDataGenerator.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include <geometry_msgs/Twist.h>
+#include <ocs2_centroidal_model/CentroidalModelRbdConversions.h>
+#include <ocs2_legged_robot/common/Types.h>
 #include <ocs2_mpc/SystemObservation.h>
 #include <ocs2_ros_interfaces/command/TargetTrajectoriesRosPublisher.h>
 
@@ -18,24 +20,45 @@ class MotionDataGenerator final {
   using json = nlohmann::json;
 
  public:
-  MotionDataGenerator(ros::NodeHandle& nh, const std::string& topicPrefix, const std::string& taskFile, const std::string& urdfFile,
-                      const std::string& referenceFile);
+  struct RecordTask {
+   public:
+    scalar_t trajTime;
+    std::string trajName;
+    std::vector<vector_t> traj;
+    scalar_t frameDuration;
+    vector3_t middleTimeVel;  // lin_vel_x, lin_vel_y, yaw_rate
 
-  void generate(const vector_t& cmdVel, scalar_t duration);
+    scalar_t motionWeight;
+    std::string recordFilePath;
+  };
+
+  MotionDataGenerator(ros::NodeHandle& nh, const std::string& topicPrefix, const std::string& taskFile, const std::string& urdfFile,
+                      const std::string& referenceFile, std::vector<RecordTask>& tasks);
+
+  void observationCallback(const ocs2_msgs::mpc_observation::ConstPtr& msg);
+
+  void generate();
 
   void saveData(const std::vector<vector_t>& dataFrames);
 
   vector_t convertDataFormat(const SystemObservation& observation);
 
  private:
+  std::vector<vector_t> recordDatas_;
+  std::vector<RecordTask> recordTasks_;
+  mutable bool beginRecord_;
+  scalar_t beginRecordTime_;
+
   ::ros::Publisher cmdVelPub_;
   ::ros::Subscriber obsSub_;
 
   mutable std::mutex latestObservationMutex_;
+  mutable size_t currentTaskIndex_;
   SystemObservation latestObservation_;
 
   CentroidalModelInfo info_;
   std::unique_ptr<PinocchioEndEffectorKinematics> eeKinematics_;
   std::unique_ptr<PinocchioInterface> pinocchioInterfacePtr_;
+  std::shared_ptr<CentroidalModelRbdConversions> rbdConversions_;
 };
 }  // namespace legged
--- legged_controllers/src/MotionDataGenerator.cpp
+++ legged_controllers/src/MotionDataGenerator.cpp
@@ -5,7 +5,6 @@
 #include <pinocchio/fwd.hpp>  // forward declarations must be included first.
 
 #include <pinocchio/algorithm/frames.hpp>
-#include <pinocchio/algorithm/jacobian.hpp>
 #include <pinocchio/algorithm/kinematics.hpp>
 
 #include <ocs2_centroidal_model/AccessHelperFunctions.h>
@@ -13,8 +12,6 @@
 #include <ocs2_centroidal_model/FactoryFunctions.h>
 #include <ocs2_centroidal_model/ModelHelperFunctions.h>
 #include <ocs2_pinocchio_interface/PinocchioEndEffectorKinematics.h>
-
-#include <pluginlib/class_list_macros.hpp>
 
 #include <ocs2_legged_robot/common/ModelSettings.h>
 #include <ocs2_robotic_tools/common/RotationTransforms.h>
@@ -25,7 +22,8 @@
 namespace legged {
 using namespace ocs2;
 MotionDataGenerator::MotionDataGenerator(ros::NodeHandle& nh, const std::string& topicPrefix, const std::string& taskFile,
-                                         const std::string& urdfFile, const std::string& referenceFile) {
+                                         const std::string& urdfFile, const std::string& referenceFile, std::vector<RecordTask>& tasks)
+    : recordTasks_(std::move(tasks)), currentTaskIndex_(0), beginRecord_(false) {
   ModelSettings modelSettings;
   modelSettings = loadModelSettings(taskFile, "model_settings", false);
 
@@ -44,67 +42,91 @@ MotionDataGenerator::MotionDataGenerator(ros::NodeHandle& nh, const std::string&
   eeKinematics_ =
       std::make_unique<PinocchioEndEffectorKinematics>(*pinocchioInterfacePtr_, pinocchioMapping, modelSettings.contactNames3DoF);
   eeKinematics_->setPinocchioInterface(*pinocchioInterfacePtr_);
+  // CentroidalModelRbdConversions
+  rbdConversions_ = std::make_shared<CentroidalModelRbdConversions>(*pinocchioInterfacePtr_, info_);
 
   // command velocity publisher
   cmdVelPub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   // observation subscriber
-  auto observationCallback = [this](const ocs2_msgs::mpc_observation::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(latestObservationMutex_);
-    latestObservation_ = ros_msg_conversions::readObservationMsg(*msg);
-  };
-  obsSub_ = nh.subscribe<ocs2_msgs::mpc_observation>(topicPrefix + "_mpc_observation", 1, observationCallback);
+  obsSub_ = nh.subscribe<ocs2_msgs::mpc_observation>(topicPrefix + "_mpc_observation", 1, &MotionDataGenerator::observationCallback, this);
 }
 
-void MotionDataGenerator::generate(const vector_t& cmdVel, scalar_t duration) {
-  while (latestObservation_.time == 0) {
-    ros::spinOnce();
-  }
-  auto startTime = std::chrono::steady_clock::now();
-  auto endTime = startTime + std::chrono::duration<scalar_t>(duration);
+void MotionDataGenerator::observationCallback(const ocs2_msgs::mpc_observation::ConstPtr& msg) {
+  std::lock_guard<std::mutex> lock(latestObservationMutex_);
 
-  scalar_t frequency = 50.0;  // Hz
-  std::chrono::microseconds period(static_cast<int>(1e6 / frequency));
+  if (beginRecord_) {
+    const auto curObservation = ros_msg_conversions::readObservationMsg(*msg);
 
-  std::vector<vector_t> dataFrames;
+    const scalar_t lastObsTime = latestObservation_.time;
+    const scalar_t curObsTime = curObservation.time;
+    assert(lastObsTime <= curObsTime);
 
-  while (std::chrono::steady_clock::now() < endTime) {
-    auto loopStartTime = std::chrono::steady_clock::now();
-
-    geometry_msgs::Twist msg;
-    msg.linear.x = cmdVel(0);
-    msg.linear.y = cmdVel(1);
-    msg.linear.z = cmdVel(2);
-    msg.angular.x = 0;
-    msg.angular.y = 0;
-    msg.angular.z = cmdVel(3);
-    cmdVelPub_.publish(msg);
-
-    vector_t dataFrame = convertDataFormat(latestObservation_);
-    dataFrames.push_back(dataFrame);
-    std::cerr << "latest time : " << latestObservation_.time << std::endl;
-
-    ros::spinOnce();
-
-    auto loopEndTime = std::chrono::steady_clock::now();
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(loopEndTime - loopStartTime);
-
-    if (elapsedTime < period) {
-      std::this_thread::sleep_for(period - elapsedTime);
+    if (curObsTime - lastObsTime >= recordTasks_[currentTaskIndex_].frameDuration) {
+      vector_t dataFrame = convertDataFormat(curObservation);
+      recordTasks_[currentTaskIndex_].traj.push_back(dataFrame);
+      latestObservation_ = curObservation;
     }
   }
+}
 
-  saveData(dataFrames);
+void MotionDataGenerator::generate() {
+  for (size_t i = 0; i < recordTasks_.size(); i++) {
+    currentTaskIndex_ = i;
+
+    auto startTime = std::chrono::steady_clock::now();
+    auto endTime = startTime + std::chrono::duration<scalar_t>(recordTasks_[i].trajTime);
+    scalar_t frequency = 100.0;  // hz
+    scalar_t dt = 1 / frequency;
+    std::chrono::microseconds period(static_cast<int>(1e6 / frequency));
+
+    vector3_t accel = recordTasks_[i].middleTimeVel / (recordTasks_[i].trajTime / 2);
+    vector3_t cmdVel(0.0, 0.0, 0.0);
+    std::cerr << "\n" << accel << std::endl;
+
+    beginRecord_ = true;
+    while (std::chrono::steady_clock::now() < endTime) {
+      auto loopStartTime = std::chrono::steady_clock::now();
+      if (endTime - loopStartTime >= std::chrono::duration<scalar_t>(recordTasks_[i].trajTime / 2)) {
+        cmdVel += accel * dt;
+      } else {
+        cmdVel -= accel * dt;
+      }
+
+      geometry_msgs::Twist msg;
+      msg.linear.x = cmdVel(0);
+      msg.linear.y = cmdVel(1);
+      msg.linear.z = 0;
+      msg.angular.x = 0;
+      msg.angular.y = 0;
+      msg.angular.z = cmdVel(2);
+      cmdVelPub_.publish(msg);
+
+      ros::spinOnce();
+      auto loopEndTime = std::chrono::steady_clock::now();
+      auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(loopEndTime - loopStartTime);
+
+      if (elapsedTime < period) {
+        std::this_thread::sleep_for(period - elapsedTime);
+      }
+    }
+    beginRecord_ = false;
+    saveData(recordTasks_[currentTaskIndex_].traj);
+  }
 }
 
 vector_t MotionDataGenerator::convertDataFormat(const SystemObservation& observation) {
-  vector_t basePos = observation.state.head(3);
-  vector_t zyxAngle = observation.state.segment<3>(3);
+  const auto& state = observation.state;
+  const auto& input = observation.input;
+
+  vector_t basePos = centroidal_model::getBasePose(state, info_).head(3);
+  vector_t zyxAngle = centroidal_model::getBasePose(state, info_).tail(3);
   Eigen::Quaternion<scalar_t> quat = getQuaternionFromEulerAnglesZyx<scalar_t>(zyxAngle);
   matrix_t inverseRot = getRotationMatrixFromZyxEulerAngles<scalar_t>(zyxAngle).inverse();
-  vector_t linVel = inverseRot * observation.state.head(3);
-  vector_t angVel = inverseRot * observation.state.segment<3>(3);
-  vector_t jointPos = observation.state.segment<12>(12);
-  vector_t jointVel = observation.input.segment<12>(12);
+  vector_t rbdState = rbdConversions_->computeRbdStateFromCentroidalModel(state, input);
+  vector_t linVel = inverseRot * rbdState.segment<3>(info_.generalizedCoordinatesNum + 3);
+  vector_t angVel = inverseRot * rbdState.segment<3>(info_.generalizedCoordinatesNum);
+  vector_t jointPos = centroidal_model::getJointAngles(state, info_);
+  vector_t jointVel = centroidal_model::getJointVelocities(input, info_);
 
   const auto& model = pinocchioInterfacePtr_->getModel();
   auto& data = pinocchioInterfacePtr_->getData();
@@ -142,7 +164,7 @@ void MotionDataGenerator::saveData(const std::vector<vector_t>& dataFrames) {
   double frameDuration = 0.021;
   bool enableCycleOffsetPosition = true;
   bool enableCycleOffsetRotation = true;
-  double motionWeight = 0.5;
+  double motionWeight = recordTasks_[currentTaskIndex_].motionWeight;
 
   json j;
   j["LoopMode"] = loopMode;
@@ -160,7 +182,7 @@ void MotionDataGenerator::saveData(const std::vector<vector_t>& dataFrames) {
     arrayJson.push_back(singleFrameJson);
   }
   j["Frames"] = arrayJson;
-  std::ofstream file("/home/luohx/catkin_ws/output/motion_record_output.json");
+  std::ofstream file(recordTasks_[currentTaskIndex_].recordFilePath);
   if (!file) {
     std::cerr << "Error: could not open file for writing.\n";
   } else {
@@ -169,7 +191,7 @@ void MotionDataGenerator::saveData(const std::vector<vector_t>& dataFrames) {
       std::cerr << "Error: failed to write to file.\n";
     }
   }
-  std::cerr << "record file finsish" << std::endl;
+  std::cerr << "record file: " << recordTasks_[currentTaskIndex_].recordFilePath << " finsish" << std::endl;
 }
 
 }  // namespace legged
@@ -190,10 +212,41 @@ int main(int argc, char** argv) {
   nodeHandle.getParam("/taskFile", taskFile);
   nodeHandle.getParam("/referenceFile", referenceFile);
 
-  MotionDataGenerator motionDataGenerator(nodeHandle, robotName, taskFile, urdfFile, referenceFile);
-  vector_t cmdVel(4);
-  cmdVel << 0.5, 0, 0, 0;
-  motionDataGenerator.generate(cmdVel, 30);
+  scalar_t totalTime = 30.0;  // s
+  std::vector<std::string> trajNames = {"forward",       "backward",       "lateral_left",       "lateral_right",
+                                        "left_steering", "right_steering", "combined_locomotion"};
+  size_t trajNum = trajNames.size();
+  scalar_t trajTime = totalTime / trajNum;
+  std::pair<scalar_t, scalar_t> linxVelRange{-1.0, 1.0};  // min max [m/s]
+  std::pair<scalar_t, scalar_t> linyVelRange{-0.5, 0.5};  // min max [m/s]
+  std::pair<scalar_t, scalar_t> yawRateRange{-1, 1};      // min max [rad/s]
+  std::vector<vector3_t> middleTimeVels = {
+      vector3_t{linxVelRange.second, 0, 0},
+      vector3_t{linxVelRange.first, 0, 0},
+      vector3_t{0, linyVelRange.second, 0},
+      vector3_t{0, linyVelRange.first, 0},
+      vector3_t{0, 0, yawRateRange.second},
+      vector3_t{0, 0, yawRateRange.first},
+      vector3_t{linxVelRange.second, linyVelRange.second, yawRateRange.second},
+  };
 
+  std::vector<MotionDataGenerator::RecordTask> recordTasks;
+  recordTasks.reserve(trajNum);
+
+  for (size_t i = 0; i < trajNum; i++) {
+    MotionDataGenerator::RecordTask task;
+    task.trajName = trajNames[i];
+    task.trajTime = trajTime;
+    task.frameDuration = 0.02;
+    task.middleTimeVel = middleTimeVels[i];
+    task.motionWeight = 0.5;
+    task.recordFilePath = "/home/luohx/catkin_ws/output/" + trajNames[i] + ".json";
+    recordTasks.push_back(task);
+  }
+
+  MotionDataGenerator motionDataGenerator(nodeHandle, robotName, taskFile, urdfFile, referenceFile, recordTasks);
+  motionDataGenerator.generate();
+
+  ::ros::spin();
   return 0;
 }
